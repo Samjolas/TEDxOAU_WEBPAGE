334bc3296402c36e390e75d6d3235a98cec2ccbf
Samuel Jolayemi <damoladfrosh@gmail.com>
2022-06-26 00:47:51 +0100

    update

--- vendor/js/script.js
+++ vendor/js/script.js
@@ -1,3 +1,6 @@
+
+// TEDxOAU TEXT ANIMATION //
+
 document.addEventListener('DOMContentLoaded',function(event){
   // array with texts to type in typewriter
   var dataText = ["TEDxOAU"];
@@ -40,3 +43,46 @@ document.addEventListener('DOMContentLoaded',function(event){
   // start the text animation
   StartTextAnimation(0);
 });
+
+
+// POPUP EFFECT //
+
+'use strict'
+
+let teamPreview = document.querySelector('.preview');
+let teamPreviewBox = document.querySelectorAll('.preview__body');
+
+document.querySelectorAll('.team__group .team__item').forEach(team__item =>{
+    team__item.addEventListener('click', function(){
+        teamPreview.style.display = 'flex'
+        let name = team__item.getAttribute('data-name');
+        teamPreviewBox.forEach(team__preview =>{
+            let target = team__preview.getAttribute('data-target');
+            if(name == target){
+                team__preview.classList.add('active');
+            }else{
+                team__preview.classList.remove('active')
+            }
+            
+        })
+    });
+});
+
+teamPreviewBox.forEach(closeModal => {
+    closeModal.querySelector('.btn-close').onclick = () =>{
+        closeModal.classList.remove('active');
+        teamPreview.style.display = 'none'
+    }
+});
+
+// MOVE TO NEXT //
+
+let Blog = document.querySelector('.blog__wrapper');
+let Next = document.getElementById ('next');
+
+Next.addEventListener('click', function(){
+  Blog.classList.add('.de-active')
+  
+})
+)
+
